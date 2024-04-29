9e632706a42cc316ab2b0a16a5442f2bc80f48b5
Milan Kordic <milan.kordic94@gmail.com>
2024-04-28 20:07:30 -0400

    Remove redundant BackendDevice import in yolo_v5.py script

--- benchmark/models/yolo_v5/yolo_v5.py
+++ benchmark/models/yolo_v5/yolo_v5.py
@@ -25,8 +25,6 @@ def yolo_v5(training: bool, task: str, config: str, microbatch: int, device: str
             compiler_cfg.balancer_policy = "Ribbon"
             os.environ["PYBUDA_RIBBON2"] = "1"
 
-        from pybuda._C.backend_api import BackendDevice
-
         available_devices = pybuda.detect_available_devices()
         if available_devices[0] == BackendDevice.Wormhole_B0:
             os.environ["PYBUDA_SUPRESS_T_FACTOR_MM"] = "49"
